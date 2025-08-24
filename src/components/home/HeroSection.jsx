6b1276309b4be749c74944f3c107f3682eaf0934
Oduoye Ayomide <ayomidecoder@gmail.com>
2025-08-24 20:54:38 +0100

    Completed the hero-section

---
+++ src/components/home/HeroSection.jsx
@@ -0,0 +1,35 @@
+import buttonIcon from '../../assets/images/button-icon-1.svg'
+
+const HeroSection = () => {
+  return (
+    <section id="hero-section" className="bg-cover bg-center h-110 md:h-screen flex">
+      <div className="text-white p-4 sm:mb-20 md:ml-10 flex flex-col gap-y-5 justify-end w-full mb-4">
+        <div>
+          <h1 className="font-playfair font-[500] text-4xl lg:text-6xl">
+            Streamline your <br /> business with smart <br /> inventory and analytics 
+          </h1>
+        </div>
+        
+        <div>
+          <p className="text-white text-[15px] font-inter md:text-[17px]">
+            Empower your business with advanced{" "}
+              <span className="relative z-10">inventory</span>
+            management and analytics.
+          </p>
+        </div>
+          
+        <div className="flex gap-x-2">
+          <button className="flex bg-[#FF4C05] cursor-pointer hover:bg-[#c25124] px-2 place-content-center pt-2 gap-x-2 rounded font-montserrat text-[15px] text-white transition-all duration-300 group">
+            <img src={buttonIcon} alt="button-icon" className="w-5 h-5  transform group-hover:translate-x-1 transition-all duration-300" />
+            Start 14 days Free Trial
+          </button>
+          <button className="border py-2 px-3 cursor-pointer rounded text-[15px] font-[500] font-montserrat transition-all duration-300 hover:border-black ">
+            Contact Us
+          </button>
+        </div>
+      </div>
+    </section>
+  )
+} 
+
+export default HeroSection
