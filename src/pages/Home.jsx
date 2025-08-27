949a328915a4d55ca387c9d3232a7fae68210847
Oduoye Ayomide <ayomidecoder@gmail.com>
2025-08-27 23:30:10 +0100

    Added Victors code and completed the footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,8 @@
 import React from 'react'
-import HeroSection from '../components/home/HeroSection'
-import TrustAndSupport from '../components/home/TrustAndSupport'
-import HeroSection2 from '../components/home/HeroSection2'
-import ManageStore from '../components/home/ManageStore'
 
 const Home = () => {
   return (
-    <div>
-      <HeroSection/>
-      <TrustAndSupport/>
-      <HeroSection2/>
-      <ManageStore/>
-    </div>
+    <div>Home</div>
   )
 }
 
